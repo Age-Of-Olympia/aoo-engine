acbb72224ddce642e0fddfc5b0a1c63c48a700c7
Léo Lavaud <leo.lavaud@orange.fr>
2024-07-11 20:59:39 +0400

    console require login & isAdmin option

--- console.php
+++ console.php
@@ -6,6 +6,31 @@ require_once('config.php');
 require_once('config/config-console.php');
 
 
+// check session
+if(!isset($_SESSION['playerId'])){
+
+    echo 'login required';
+    exit();
+}
+
+
+// check admin (only once per session)
+if(!isset($_SESSION['isAdmin'])){
+
+    // check admin
+    $player = new Player($_SESSION['playerId']);
+    if(!$player->have_option('isAdmin')){
+
+        echo 'admin account required';
+        exit();
+    }
+    else{
+
+        $_SESSION['isAdmin'] = true;
+    }
+}
+
+
 // get cmd history
 if(!empty($_POST['cmdHistory'])){
 
