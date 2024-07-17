729b9f93c5743b9dd7d48682d14a91865ddd7b01
ceddoc <cedboss05@hotmail.com>
2024-07-17 16:06:40 +0200

    faction base implementation

--- classes/console-commands/playercmd.php
+++ classes/console-commands/playercmd.php
@@ -5,15 +5,17 @@ class PlayerCmd extends Command
     public function __construct() {
         parent::__construct("player",[new Argument('action',false), new Argument('mat',false), new Argument('option1',true), new Argument('option2',true)]);
         parent::setDescription(<<<EOT
-Manipule la table "players".
+Manipule la table "players et les éléments associés à un player
 Exemple:
 > player create Orcrist olympien
 > player create Ocyrhoée elfe,pnj
-> player edit Finn race,lutin
-> player edit 1 name,Léo
+> player edit Finn race lutin
+> player edit 1 name Léo
 > player additem Orcrist or 100
 > player additem 1 1 -100
 > player addbank Orcrist pierre 100
+> player addlog Orcrist "this is a log to be added"
+> player deletelastlog Orcrist
 EOT);
     }
 
@@ -24,236 +26,256 @@ EOT);
 
 
         if($action == 'create'){
-
-
-            $name = $argumentValues[1];
-
-
-            $optList = explode(',', $argumentValues[2]);
-
-
-            $pnj = false;
-
-
-            if(count($optList) == 1){
-
-                list($race) = $optList;
-            }
-            elseif(count($optList) == 2){
-
-                list($race, $pnj) = $optList;
-            }
-            else{
-
-                return 'invalid option ('. $argumentValues[2] .'). Must be: "race" or "race,pnj", ie. "elfe" or "lutin,pnj"';
-            }
-
-
-            if(!$raceJson = json()->decode('races', $race)){
-
-                return '<font color="red">invalid race ('. $race .')</font>';
-            }
-
-
-            $lastId = Player::put_player($name, $race, $pnj);
-
-
-            $pnjTxt = ($pnj) ? 'pnj=true' : 'pnj=false';
-
-
-            return 'player '. $name .' created ('. $raceJson->name .', '. $pnjTxt .') <a href="#" OnClick="document.getElementById(\'input-line\').value = \'session open '. $lastId .'\'; document.getElementById(\'input-line\').focus()">mat: '. $lastId .'</a>';
+            return create_player($argumentValues);
         }
 
-        else{
+        $player=parent::getPlayer($argumentValues[1]);
+
+        $player->get_data();
+
+        if($action == 'edit'){
+            return edit_player($argumentValues, $player);
+        }
+
+        if($action == 'purge'){
+            return purge_player($argumentValues, $player);
+        }
 
 
-            $player=parent::getPlayer($argumentValues[1]);
+        if($action == 'additem' || $action == 'addbank'){
+            return add_item($argumentValues, $player);
 
-            $player->get_data();
-
-
-            if($action == 'edit'){
-
-
-                if(!isset($argumentValues[2])){
-
-                    return '<font color="red">invalid argument option1 ('. $argumentValues[2] .').<br />
-                    Usage: player edit [mat] [field] [value] ie player edit Orcrist name "Orcrist le Vénérable"</font>';
-                }
-
-                if(!isset($argumentValues[3])){
-
-                    return '<font color="red">invalid argument option2 ('. $argumentValues[3] .').<br />
-                    Usage: player edit [mat] [field] [value] ie player edit Orcrist name "Orcrist le Vénérable"</font>';
-                }
-
-
-                $field = $argumentValues[2];
-
-                $value = $argumentValues[3];
-
-
-                if(in_array($field, array('id','coords_id','mail','psw','ip'))){
-
-                    return '<font color="orange">field "'. $field .'" is protected</font>';
-                }
-
-
-                if(!isset($player->data->$field)){
-
-                    return '<font color="red">invalid field option ('. $field .') does not exists.</font>';
-                }
-
-                if(is_numeric($player->data->$field) && !is_numeric($value)){
-
-                    return '<font color="red">invalid value option ('. $value .') this field require numeric value.</font>';
-                }
-
-
-                $sql = '
-                UPDATE players
-                SET
-                `'. $field .'` = ?
-                WHERE
-                id = ?
-                ';
-
-                $db = new Db();
-
-                $sql = $db->exe($sql, array($value, $player->id));
-
-
-                $player->refresh_data();
-
-
-                return 'player '. $player->data->name .': field "'. $field .'" changed to value "'. $value .'"';
-            }
-
-            if($action == 'purge'){
-
-
-                if($argumentValues[2] == 'view'){
-
-                    $files = glob('datas/private/players/'. $player->id .'.svg');
-                }
-                elseif($argumentValues[2] == 'all'){
-
-                    $files = glob('datas/private/players/'. $player->id .'*');
-                }
-                elseif($argumentValues[2] == 'allplayers'){
-
-                    $files = glob('datas/private/players/*');
-                }
-
-                ob_start();
-
-                foreach($files as $file){
-
-                    @unlink($file);
-                    echo $file;
-                }
-
-                $return = ob_get_clean();
-
-                return 'player '. $player->data->name .': '. $argumentValues[2] .' cache purged '. $return;
-            }
-
-
-            if($action == 'additem' || $action == 'addbank'){
-
-
-                if(!isset($argumentValues[2])){
-
-                    return '<font color="red">error missing option1 [item id or name]. usage: player additem [mat] [existing item id or name] [number]</font>';
-                }
-
-                if(!isset($argumentValues[3])){
-
-                    return '<font color="red">error missing option2 [number]. usage: player additem [mat] [existing item id or name] [number]</font>';
-                }
-
-                ob_start();
-
-                if(is_numeric($argumentValues[2])){
-
-                    $item = new Item($argumentValues[2]);
-                }
-                else{
-
-                    $item = Item::get_item_by_name($argumentValues[2]);
-                }
-
-                $item->get_data();
-
-                $bank = ($action == 'addbank') ? true : false;
-
-                $place = ($action == 'addbank') ? 'bank' : 'inventory';
-
-                $item->add_item($player, $argumentValues[3], $bank);
-
-                $return = ob_get_clean();
-
-                return 'player '. $player->data->name .': '. $item->data->name .' x'. $argumentValues[3] .' added to '. $place;
-            }
         }
 
         if($action == 'addxp'){
-
-
-            if(!isset($argumentValues[2])){
-
-                return '<font color="red">error missing option1 [xp]. usage: player addxp [mat] [xp]</font>';
-            }
-
-            $player->put_xp($argumentValues[2]);
-
-            return $argumentValues[2] .'Xp et Pi ajoutés à '. $player->data->name;
-
+            return add_xp($argumentValues, $player);
         }
 
 
         if($action == 'addlog'){
-
-
-            if(!isset($argumentValues[2])){
-
-                return '<font color="red">error missing option1 [target id or name]. usage: player addlog [mat]  [target id or name] [text]</font>';
-            }
-
-            if(!isset($argumentValues[3])){
-
-                return '<font color="red">error missing option2 [text]. usage: player addlog [mat]  [target id or name] [text]</font>';
-            }
-
-            if(is_numeric($argumentValues[2])){
-
-                $target = new Player($argumentValues[2]);
-            }
-            else{
-
-                $target = Player::get_player_by_name($argumentValues[2]);
-            }
-
-
-            $target->get_data();
-
-            Log::put($player, $target, $argumentValues[3], $type="console");
-
-            return $player->data->name .' to '. $target->data->name .' log added: "'. $argumentValues[3] .'"';
+            return add_log($argumentValues,$player);
         }
 
         if($action == 'deletelastlog'){
-
-
-            $sql = 'DELETE FROM players_logs WHERE player_id = ? ORDER BY time DESC LIMIT 1';
-
-            $db = new Db();
-
-            $db->exe($sql, $player->id);
-
-            return $player->data->name .' last log deleted';
+            return delete_last_log($player);
         }
 
-        return '<font color="orange">no changes detected</font>';
+        return '<font color="orange">Action : '.$action.' unknown</font>';
     }
 }
+
+function create_player($argumentValues){
+
+    $name = $argumentValues[1];
+
+
+    $optList = explode(',', $argumentValues[2]);
+
+
+    $pnj = false;
+
+
+    if(count($optList) == 1){
+
+        list($race) = $optList;
+    }
+    elseif(count($optList) == 2){
+
+        list($race, $pnj) = $optList;
+    }
+    else{
+
+        return 'invalid option ('. $argumentValues[2] .'). Must be: "race" or "race,pnj", ie. "elfe" or "lutin,pnj"';
+    }
+
+
+    if(!$raceJson = json()->decode('races', $race)){
+
+        return '<font color="red">invalid race ('. $race .')</font>';
+    }
+
+
+    $lastId = Player::put_player($name, $race, $pnj);
+
+
+    $pnjTxt = ($pnj) ? 'pnj=true' : 'pnj=false';
+
+
+    return 'player '. $name .' created ('. $raceJson->name .', '. $pnjTxt .') <a href="#" OnClick="document.getElementById(\'input-line\').value = \'session open '. $lastId .'\'; document.getElementById(\'input-line\').focus()">mat: '. $lastId .'</a>';
+}
+
+function edit_player($argumentValues, $player){
+
+    if(!isset($argumentValues[2])){
+
+        return '<font color="red">invalid argument option1 ('. $argumentValues[2] .').<br />
+                Usage: player edit [mat] [field] [value] ie player edit Orcrist name "Orcrist le Vénérable"</font>';
+    }
+
+    if(!isset($argumentValues[3])){
+
+        return '<font color="red">invalid argument option2 ('. $argumentValues[3] .').<br />
+                Usage: player edit [mat] [field] [value] ie player edit Orcrist name "Orcrist le Vénérable"</font>';
+    }
+
+
+    $field = $argumentValues[2];
+
+    $value = $argumentValues[3];
+
+
+    if(in_array($field, array('id','coords_id','mail','psw','ip'))){
+
+        return '<font color="orange">field "'. $field .'" is protected</font>';
+    }
+
+
+    if(!isset($player->data->$field)){
+
+        return '<font color="red">invalid field option ('. $field .') does not exists.</font>';
+    }
+
+    if(is_numeric($player->data->$field) && !is_numeric($value)){
+
+        return '<font color="red">invalid value option ('. $value .') this field require numeric value.</font>';
+    }
+
+
+    $sql = '
+            UPDATE players
+            SET
+            `'. $field .'` = ?
+            WHERE
+            id = ?
+            ';
+
+    $db = new Db();
+
+    $sql = $db->exe($sql, array($value, $player->id));
+
+
+    $player->refresh_data();
+
+
+    return 'player '. $player->data->name .': field "'. $field .'" changed to value "'. $value .'"';
+}
+
+function purge_player($argumentValues, $player){
+
+
+    if($argumentValues[2] == 'view'){
+
+        $files = glob('datas/private/players/'. $player->id .'.svg');
+    }
+    elseif($argumentValues[2] == 'all'){
+
+        $files = glob('datas/private/players/'. $player->id .'*');
+    }
+    elseif($argumentValues[2] == 'allplayers'){
+
+        $files = glob('datas/private/players/*');
+    }
+
+    ob_start();
+
+    foreach($files as $file){
+
+        @unlink($file);
+        echo $file;
+    }
+
+    $return = ob_get_clean();
+
+    return 'player '. $player->data->name .': '. $argumentValues[2] .' cache purged '. $return;
+}
+
+function add_item( $argumentValues,  $player)
+{
+    if(!isset($argumentValues[2])){
+
+        return '<font color="red">error missing option1 [item id or name]. usage: player additem [mat] [existing item id or name] [number]</font>';
+    }
+
+    if(!isset($argumentValues[3])){
+
+        return '<font color="red">error missing option2 [number]. usage: player additem [mat] [existing item id or name] [number]</font>';
+    }
+
+    ob_start();
+
+    if(is_numeric($argumentValues[2])){
+
+        $item = new Item($argumentValues[2]);
+    }
+    else{
+
+        $item = Item::get_item_by_name($argumentValues[2]);
+    }
+
+    $item->get_data();
+
+    $bank = ($action == 'addbank') ? true : false;
+
+    $place = ($action == 'addbank') ? 'bank' : 'inventory';
+
+    $item->add_item($player, $argumentValues[3], $bank);
+
+    $return = ob_get_clean();
+
+    return 'player '. $player->data->name .': '. $item->data->name .' x'. $argumentValues[3] .' added to '. $place;
+
+}
+
+function add_xp($argumentValues, $player)
+{
+
+    if(!isset($argumentValues[2])){
+
+        return '<font color="red">error missing option1 [xp]. usage: player addxp [mat] [xp]</font>';
+    }
+
+    $player->put_xp($argumentValues[2]);
+
+    return $argumentValues[2] .'Xp et Pi ajoutés à '. $player->data->name;
+}
+
+function add_log($argumentValues, $player){
+
+    if(!isset($argumentValues[2])){
+
+        return '<font color="red">error missing option1 [target id or name]. usage: player addlog [mat]  [target id or name] [text]</font>';
+    }
+
+    if(!isset($argumentValues[3])){
+
+        return '<font color="red">error missing option2 [text]. usage: player addlog [mat]  [target id or name] [text]</font>';
+    }
+
+    if(is_numeric($argumentValues[2])){
+
+        $target = new Player($argumentValues[2]);
+    }
+    else{
+
+        $target = Player::get_player_by_name($argumentValues[2]);
+    }
+
+
+    $target->get_data();
+
+    Log::put($player, $target, $argumentValues[3], $type="console");
+
+    return $player->data->name .' to '. $target->data->name .' log added: "'. $argumentValues[3] .'"';
+}
+
+function delete_last_log($player){
+
+    $sql = 'DELETE FROM players_logs WHERE player_id = ? ORDER BY time DESC LIMIT 1';
+
+    $db = new Db();
+
+    $db->exe($sql, $player->id);
+
+    return $player->data->name .' last log deleted';
+}
+
---
+++ scripts/faction_list.php
@@ -0,0 +1,49 @@
+<?php
+
+
+echo '
+    <table border="1" cellspacing="0" class="marbre" align="center">
+    ';
+
+echo '
+    <tr>
+        <th></th>
+        <th>Nom</th>
+        <th>Peuple</th>
+        <th>Xp</th>
+        <th>Rang</th>
+    </tr>
+    ';
+
+while($row = $res->fetch_object()){
+
+
+    $raceJson = json()->decode('races', $row->race);
+
+    echo '
+        <tr>
+            <td>
+                <img src="'. $row->avatar .'" />
+            </td>
+            <td>
+                <a href="infos.php?targetId='.$row->id.'">'. $row->name .'</a>
+            </td>
+            <td>
+                '. $raceJson->name .'
+            </td>
+            <td>
+                '. $row->xp .'
+            </td>
+            <td>
+                '. $facJson->role[$row->factionRole]->name .'
+            </td>
+        </tr>
+        ';
+}
+
+echo '
+    </table>
+    ';
+
+
+
