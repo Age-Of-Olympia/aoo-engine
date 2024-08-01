535d2606dd2d11f352f347125abfb15c140df5bf
Léo Lavaud <leo.lavaud@orange.fr>
2024-08-01 19:33:14 +0400

    fix venerate no god

--- scripts/actions/prier.php
+++ scripts/actions/prier.php
@@ -1,11 +1,21 @@
 <?php
 
+
+if(!$player->data->godId){
+
+    exit('<font color="red">Vos prières ne servent à rien, car vous ne vénérez aucun Dieu!</font>');
+}
+
+
+$god = new Player($player->data->godId);
+$god->get_data();
+
 $pf = rand(1,3);
 
 $player->put_pf($pf);
 
 echo '
-Vous priez et gagnez '. $pf .' Points de Foi (total '. $player->data->pf .'Pf).
+Vous priez '. $god->data->name .' et gagnez '. $pf .' Points de Foi (total '. $player->data->pf .'Pf).
 
 <div class="action-details">1d3 = '. $pf .'</div>
 ';
