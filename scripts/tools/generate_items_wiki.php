d2143f2ebc9f895304fbd2eaabcfc1201ffea56c
Léo Lavaud <leo.lavaud@orange.fr>
2024-08-21 09:11:41 +0400

    fort turok

---
+++ scripts/tools/generate_items_wiki.php
@@ -0,0 +1,127 @@
+<?php
+
+echo '<textarea style="width: 100vw; height: 50vw;">';
+
+
+$artJson = json()->decode('', 'crafts');
+
+
+foreach($artJson as $occ=>$e){
+
+
+    switch($occ){
+
+
+                case 'common':
+            $occName = 'Objets Communs';
+                break;
+
+                case 'elfe':
+            $occName = 'Objets Elfiques';
+                break;
+
+                case 'geant':
+            $occName = 'Objets Géants';
+                break;
+
+                case 'nain':
+            $occName = 'Objets Nains';
+                break;
+
+                case 'olympien':
+            $occName = 'Objets Olympiens';
+                break;
+
+                case 'hs':
+            $occName = 'Objets Homme-Sauvages';
+                break;
+
+                case 'lutin':
+            $occName = 'Objets Lutins';
+                break;
+
+                case 'ressource':
+            $occName = 'Ressources';
+                break;
+
+                default:
+            $occName = false;
+                break;
+    }
+
+    if(!$occName) continue;
+
+    echo '==== '. $occName .' ====
+';
+
+
+    echo '^ # ^ Objet ^ Infos ^
+';
+
+
+    $n = 1;
+
+    foreach($e as $item){
+
+
+        $item = new Item($item->id);
+
+        $item->get_data();
+
+        $infos = Item::get_item_carac($item->data);
+
+
+        $formattedInfos = implode(' ', $infos);
+
+
+        $formattedInfos = str_replace('<font color="blue">', '', $formattedInfos);
+        $formattedInfos = str_replace('<font color="red">', '', $formattedInfos);
+        $formattedInfos = str_replace('</font>', '', $formattedInfos);
+
+
+        echo '| '. $n .' | {{https://aootest.net/img/items/'. $item->row->name .'_mini.webp}} '. $item->data->name .' | '. $formattedInfos .' |
+';
+
+
+        $n++;
+    }
+
+
+}
+
+/*
+foreach(RACES as $race){
+
+
+    $raceJson = json()->decode('races', $race);
+
+    echo '==== '. $raceJson->name .' ====
+^ # ^ Nom de l\'objet ^ Type ^ Coût ^ Description ^
+';
+
+    $n = 1;
+
+    foreach($raceJson->actionsPack as $action){
+
+
+        $actionJson = json()->decode('actions', $action);
+
+        if(!isset($actionJson->type) || $actionJson->type != 'sort'){
+
+            continue;
+        }
+
+
+        $type = (!empty($actionJson->subtype)) ? 'technique' : 'sort';
+
+
+        echo '| '. $n .' | {{https://aootest.net/img/spells/'. $action .'.jpeg}} '. $actionJson->name .' | '. $type .' | '. $actionJson->costs->pm .'PM | '. $actionJson->text .' |
+';
+
+        $n++;
+    }
+}
+*/
+
+
+echo '</textarea>';
