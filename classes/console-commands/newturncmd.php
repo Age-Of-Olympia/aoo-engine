ba4c16e3be48a483b454fc78e4a77d7d53f6823d
Firefly74940 <guiot.sidney@gmail.com>
2024-09-27 08:17:19 +0000

    added option to test antizerk with new_turn command

--- classes/console-commands/newturncmd.php
+++ classes/console-commands/newturncmd.php
@@ -3,9 +3,12 @@
 class NewTurnCmd extends Command
 {
     public function __construct() {
-        parent::__construct("new_turn",[]);
+        parent::__construct("new_turn",[new Argument('real',true)]);
         parent::setDescription(<<<EOT
-déclenche un nouveau tour pour la session en cours 
+déclenche un nouveau tour pour la session en cours. l'argument real permet de simuler un vrai tour avec non réinitialisation de l'anti-berserk. 
+Exemple:
+> new_turn
+> new_turn real
 EOT);
     }
 
@@ -13,14 +16,19 @@ EOT);
     {
         $player = new Player($_SESSION['playerId']);
 
-        $sql = 'UPDATE players SET nextTurnTime = ?, lastActionTime = 0, antiBerserkTime = 0 WHERE id = ?';
-
+        if(isset($argumentValues[0]) && $argumentValues[0] == 'real'){
+            $sql = 'UPDATE players SET nextTurnTime = ? WHERE id = ?';
+        }
+        else
+        {
+            $sql = 'UPDATE players SET nextTurnTime = ?, lastActionTime = 0, antiBerserkTime = 0 WHERE id = ?';
+        }
         $db = new Db();
 
         $db->exe($sql, array(time(), $player->id));
 
         $player->refresh_data();
 
-        return 'New turn for player '. $player->id. ' done.';
+        return 'New turn for player '. $player->id. ' done';
     }
 }
