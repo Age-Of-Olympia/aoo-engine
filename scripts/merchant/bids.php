78333a7772008b5ffc88bd1fd53a05d7b56321af
ceddoc <cedboss05@hotmail.com>
2024-09-30 12:25:19 +0200

    #151 - fix and improve offer or asks cancellation

--- scripts/merchant/bids.php
+++ scripts/merchant/bids.php
@@ -42,7 +42,7 @@ if(!empty($_GET['itemId'])){
     echo '<script>window.basePrice = '. $item->data->price .';</script>';
 
 
-    echo $market->print_detail($item, 'bids');
+    echo $market->print_detail($item, 'bids',$player->id);
 
 
     exit();
