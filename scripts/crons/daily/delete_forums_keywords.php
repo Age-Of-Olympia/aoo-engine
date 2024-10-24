d00407ece42c4fcf87e1d98088a1495ee49e318f
ceddoc <cedboss05@hotmail.com>
2024-10-24 09:50:33 +0200

    #234 - Add a clean of forum keywords in case of deleted post.

---
+++ scripts/crons/daily/delete_forums_keywords.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * ce script delete les forums_keywords si le fichier json n'existe pas
+ * */
+
+$sql = "SELECT id, postName FROM forums_keywords";
+
+$db = new Db();
+
+$res = $db->exe($sql);
+
+
+while($row = $res->fetch_object()){
+
+    $postName = $row->postName;
+
+    $filePath = 'datas/private/forum/posts/'. $postName . '.json';
+
+    // Si le fichier correspondant n'existe pas, supprimer l'entrée
+    if (!file_exists($filePath)) {
+        $deleteSql = "DELETE FROM forums_keywords WHERE id = ?";
+        $db->exe($deleteSql, [$row->id]);
+        echo "line deleted for postName: $postName <br />";
+    }
+}
+
+
+echo 'done';
