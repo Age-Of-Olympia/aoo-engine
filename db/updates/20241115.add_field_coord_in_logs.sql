b8afc2795678415332e85527a6f99c5cf3593e00
Thomas Saquet <thomas@solution-libre.fr>
2024-12-06 11:47:49 +0000

    added coords id to logs archive table

--- db/updates/20241115.add_field_coord_in_logs.sql
+++ db/updates/20241115.add_field_coord_in_logs.sql
@@ -1,5 +1,11 @@
 ALTER TABLE `players_logs`
-ADD `coords_id` int(11) NOT NULL DEFAULT '0';
+ADD `coords_id` int(11) NULL DEFAULT '0';
 ALTER TABLE `players_logs`
   ADD CONSTRAINT `players_logs_coords_fk_1` FOREIGN KEY (`coords_id`) REFERENCES `coords` (`id`);
+COMMIT;
+
+ALTER TABLE `players_logs_archives`
+ADD `coords_id` int(11) NULL DEFAULT '0';
+ALTER TABLE `players_logs_archives`
+  ADD CONSTRAINT `players_logs_archives_coords_fk_1` FOREIGN KEY (`coords_id`) REFERENCES `coords` (`id`);
 COMMIT;
