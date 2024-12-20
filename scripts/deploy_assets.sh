36c103eba6336bc9aca08b03d05213b682f87398
Thomas Saquet <thomas@solution-libre.fr>
2024-12-20 20:20:35 +0000

    fixed json list for deployments

--- scripts/deploy_assets.sh
+++ scripts/deploy_assets.sh
@@ -6,5 +6,11 @@ cp -ra ~/aoo-game-data/datas/public/factions ~/public_html/datas/public
 cp -ra ~/aoo-game-data/datas/public/items ~/public_html/datas/public
 cp -ra ~/aoo-game-data/datas/public/quests ~/public_html/datas/public
 cp -ra ~/aoo-game-data/datas/public/races ~/public_html/datas/public
+cp -ra ~/aoo-game-data/datas/public/crafts.json ~/public_html/datas/public
+
+cp -ra ~/aoo-game-data/datas/private/dialogs ~/public_html/datas/private
+cp -ra ~/aoo-game-data/datas/private/maps ~/public_html/datas/private
+cp -ra ~/aoo-game-data/datas/private/plans ~/public_html/datas/private
+cp -ra ~/aoo-game-data/datas/private/races ~/public_html/datas/private
 
 cp -ra ~/aoo-game-data/img ~/public_html/
