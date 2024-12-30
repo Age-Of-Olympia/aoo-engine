e9044c2e53be4925e3c9506aaa5ccdfbfd89e3d5
Oubould <oubould@protonmail.com>
2024-12-30 17:34:19 +0100

    [ECHANGES] Code de la branche echange-v2 de Zarik sur Github

--- scripts/merchant/exchanges.php
+++ scripts/merchant/exchanges.php
@@ -12,57 +12,106 @@ if(isset($_GET['create'])) {
     $recipient = Player::get_player_by_name($_POST['recipient']);
 
     $exchange = new Exchange();
-    $exchange->create($player->id,$recipient->id);
+    $exchange->db->start_transaction('create_exchange');
+    try {
+      $exchange->create($player->id,$recipient->id);
 
-    foreach ($objects as $object) {
-        $decodedObject = json_decode($object, true);
-        $item = new Item($decodedObject['id']);
-        $item->add_item($player, -$decodedObject['n'], true);
-        $exchange->add_item_to_exchange($item->id, $decodedObject['n']);
+      foreach ($objects as $object) {
+          $decodedObject = json_decode($object, true);
+          $item = new Item($decodedObject['id']);
+          $count = abs($decodedObject['n']);
+          if(!$item->add_item($player, -$count, true))
+          {
+            throw new Exception('Erreur lors de l\'ajout de l\'objet à l\'échange');
+          }
+          $exchange->add_item_to_exchange($item->id, $count,$player->id);
+      }
+    } catch (Throwable $th) {
+      $exchange->db->rollback_transaction('create_exchange');
+      ExitError('Erreur lors de la création de l\'échange');
     }
+    $exchange->db->commit_transaction('create_exchange');
+    ExitSuccess($exchange->db->get_last_id("items_exchanges"));
   }
+  exit();
 }
 
 if(isset($_GET['accept']) || isset($_GET['refuse'])) {
   $exchangeId= !empty($_GET['accept']) ? $_GET['accept'] : $_GET['refuse'];
   $exchange = new Exchange($exchangeId);
-  $exchange->get_base_data();
-  if ($player->id != $exchange->targetId ){
-    exit('Current player is not the target of the exchange');
+  if(!$exchange->is_in_progress()){
+    exit('Echange déjà cloturé');
   }
-  $offeringPlayer = new Player($exchange->playerId);
-  $offeringPlayer->get_data();
-  if(isset($_GET['accept'])){
-    $exchange->get_items_data();
-    $exchange->give_items($player);
-    $exchange->accept_exchange();
-    echo "<b>Vous avez accepté l'échange proposé par ".$offeringPlayer->data->name." </b>";
+  $exchange->db->start_transaction('accept_or_reffuse_exchange');
+  try {
+    $exchange->get_base_data();
+    if ($player->id != $exchange->targetId && $player->id != $exchange->playerId){
+      exit('Current player is not part of the exchange');
+    }
+    $isTarget = $player->id == $exchange->targetId;
+
+    $offeringPlayer = new Player(  $isTarget ? $exchange->playerId : $exchange->targetId);
+    $targetPlayer = new Player( $isTarget ? $exchange->targetId:$exchange->playerId );
+    $offeringPlayer->get_data();
+    if(isset($_GET['accept'])){
+      if(!$exchange->is_in_progress())exit('echange n\'est plus de l\'actualité');
+      $exchange->accept_exchange($isTarget);
+      echo "<b>Vous avez accepté l'échange proposé par ".$offeringPlayer->data->name." </b>";
+      if($exchange->playerOk == 1 && $exchange->targetOk == 1){
+        echo "<b>Vous avez validé l'échange proposé par ".$offeringPlayer->data->name." </b>";
+
+        $exchange->give_items(from_player:$offeringPlayer , to_player:$targetPlayer);
+        $exchange->give_items(from_player:$targetPlayer , to_player:$offeringPlayer);
+    
+      }
+    }
+    else if(isset($_GET['refuse'])){
+      $exchange->refuse_exchange(Istarget:$isTarget,IsPlayer:!$isTarget);
+      echo "<b>Vous avez refusé l'échange proposé par ".$offeringPlayer->data->name." </b>";
+    }
+  } catch (Throwable $th) {
+    $exchange->db->rollback_transaction('create_exchange');
+    exit('Erreur lors de l\'acceptation/refus de l\'échange');
   }
-  if(isset($_GET['refuse'])){
-    $exchange->get_items_data();
-    $exchange->give_items($offeringPlayer);
-    $exchange->refuse_exchange();
-    echo "<b>Vous avez refusé l'échange proposé par ".$offeringPlayer->data->name." </b>";
-  }
+  $exchange->db->commit_transaction('accept_or_reffuse_exchange');
 }
 
 
 if(isset($_GET['cancel']) ) {
   $exchange = new Exchange($_GET['cancel']);
   $exchange->get_base_data();
-  if ($player->id !== $exchange->playerId ){
-    exit('Current player is not the source of the exchange');
+  if(!$exchange->is_in_progress()){
+    exit('Echange déjà cloturé');
   }
-  $exchange->get_items_data();
-  $offeringPlayer = new Player($exchange->playerId);
-  $exchange->give_items($offeringPlayer);
-  $exchange->cancel_exchange();
+  $exchange->db->start_transaction('cancel_exchange');
+  try {
+    if ($player->id !== $exchange->playerId && $player->id !== $exchange->targetId){
+      exit('Current player is not the part of the exchange');
+    }
+    $exchange->get_items_data();
+    $offeringPlayer = new Player($exchange->playerId);
+    $targetPlayer = new Player($exchange->targetId);
+    //refund items
+    $exchange->give_items(from_player:$offeringPlayer , to_player:$offeringPlayer);
+    $exchange->give_items(from_player:$targetPlayer , to_player:$targetPlayer);
 
+    $exchange->cancel_exchange();
+  } catch (Throwable $th) {
+    $exchange->db->rollback_transaction('cancel_exchange');
+    exit('Erreur lors de l\'annulation de l\'échange');
+  }
+  $exchange->db->commit_transaction('cancel_exchange');
 }
 
 if(isset($_GET['newExchange'])){
 
     include('scripts/merchant/new_exchange.php');
+
+    exit();
+}
+if(isset($_GET['editExchange'])){
+
+    //include('scripts/merchant/exchanges.php');
 
     exit();
 }
@@ -83,31 +132,55 @@ echo '<div>Pour échanger des objets avec d\'autres personnages par le biais des
           if ($exchange->playerId != $player->id){
               $fromPlayer = new Player($exchange->playerId);
               $fromPlayer->get_data();
-              echo 'Echange reçu de '.$fromPlayer->data->name. ' le '.date('d/m/Y H:i', $exchange->updateTime) . '. 
-              <a href="merchant.php?targetId='.$target->id.'&exchanges&accept='.$exchange->id.'">Accepter</a> - 
-              <a href="merchant.php?targetId='.$target->id.'&exchanges&refuse='.$exchange->id.'">Refuser</a> <br/>';
-              foreach ($exchange->items as $exchangeItem) {
-                $item  = new Item($exchangeItem->item_id);
-                $item->get_data();
-                echo '<li>'. $exchangeItem->n . ' ' . $item->data->name. '</li>';
+              echo '<section style="background-color: #f0f8ff5e;">';
+              echo 'Echange reçu de <a href="infos.php?targetId='.$fromPlayer->id.'">'.$fromPlayer->data->name.'('. $fromPlayer->id .')</a> le '.date('d/m/Y H:i', $exchange->updateTime) . '. 
+              <br> L\'échange sera validé quand les deux joueurs auront accepté.<br>';
+              if($exchange->playerOk == 1){
+                echo $targetPlayer->data->name. 'a accepté';
               }
+              if($exchange->targetOk == 1){
+               echo 'Vous avez accepté. => <a href="merchant.php?targetId='.$target->id.'&exchanges&refuse='.$exchange->id.'">Refuser</a> ( n\'annule pas l\'échange)<br>';
+              }
+              else
+                echo '<a href="merchant.php?targetId='.$target->id.'&exchanges&accept='.$exchange->id.'">Accepter l\'échange</a><br>';
+              echo '<a href="merchant.php?targetId='.$target->id.'&exchanges&cancel='.$exchange->id.'">Annuler ( suprimer )</a><br>';
+              echo '<a href="merchant.php?targetId='.$target->id.'&exchanges&editExchange='.$exchange->id.'">Modifier</a> <br>';
+              echo '<ul class="compact-list">
+              <li style="font-weight: bold;">Vous recevez : </li>';
+              echo $exchange->render_items_for_player($exchange->playerId);
+              echo '<li style="font-weight: bold;">Vous donnez : </li>';
+              echo $exchange->render_items_for_player(  $exchange->targetId);
               echo '</ul> <br/>';
+              echo '</section>';
           }
         }
         echo '<br/>';
         foreach ($exchanges as $exchange) {
             if ($exchange->playerId == $player->id){
+                echo '<section style="background-color: #f0f8ff5e;">';
                 $targetPlayer = new Player($exchange->targetId);
                 $targetPlayer->get_data();
-                echo 'Echange proposé à '.$targetPlayer->data->name. ' le '.date('d/m/Y H:i', $exchange->updateTime). '.
-                <a href="merchant.php?targetId='.$target->id.'&exchanges&cancel='.$exchange->id.'">Annuler</a> 
-                <br><ul class="compact-list">';
-                foreach ($exchange->items as $exchangeItem) {
-                  $item  = new Item($exchangeItem->item_id);
-                  $item->get_data();
-                  echo '<li>'. $exchangeItem->n . ' ' . $item->data->name. '</li>';
+                echo 'Echange proposé à <a href="infos.php?targetId='.$targetPlayer->id.'">'.$targetPlayer->data->name.'('. $targetPlayer->id .')</a> le '.date('d/m/Y H:i', $exchange->updateTime). '.
+                <br> L\'échange sera validé quand les deux joueurs auront accepté.<br>';
+
+                if($exchange->targetOk == 1){
+                    echo $targetPlayer->data->name. 'a accepté';
                 }
+                if($exchange->playerOk == 1){
+                 echo 'Vous avez accepté. => <a href="merchant.php?targetId='.$target->id.'&exchanges&refuse='.$exchange->id.'">Refuser</a> ( n\'annule pas l\'échange) <br>';
+                }
+                else 
+                  echo '<a href="merchant.php?targetId='.$target->id.'&exchanges&accept='.$exchange->id.'">Accepter l\'échange</a> <br>';
+               
+                echo '<a href="merchant.php?targetId='.$target->id.'&exchanges&cancel='.$exchange->id.'">Annuler ( suprimer )</a><br>'; 
+                echo '<a href="merchant.php?targetId='.$target->id.'&exchanges&editExchange='.$exchange->id.'">Modifier</a>  <br>';
+                echo '<br><ul class="compact-list">
+                <li style="font-weight: bold;">Vous recevez : </li>';
+                echo $exchange->render_items_for_player($exchange->targetId);
+                echo '<li style="font-weight: bold;">Vous donnez : </li>';
+                echo $exchange->render_items_for_player( $exchange->playerId);
                 echo '</ul> <br/>';
+                echo '</section>';
             }
         }
     ?>
@@ -120,7 +193,3 @@ echo '<div>Pour échanger des objets avec d\'autres personnages par le biais des
   </div>
 
 </div>
-
-
-
-
