6d2b9743c7c4c55527e68a5dacb36fb39e277638
Thomas Saquet <thomas@solution-libre.fr>
2025-01-03 21:00:24 +0000

    added form to upload images for avatars and portraits

---
+++ ajax/account/get_next_number.php
@@ -0,0 +1,38 @@
+<?php
+
+include ($_SERVER['DOCUMENT_ROOT'].'/checks/admin-check.php');
+
+use App\Entity\EntityManagerFactory;
+use App\Entity\Race;
+use App\Enum\ImageType;
+
+$entityManager = EntityManagerFactory::getEntityManager();
+
+header('Content-Type: application/json');
+
+// 1) Read query params (e.g., /get_next_number.php?type=portrait&raceId=2)
+$type   = $_GET['type']   ?? '';
+$raceId = $_GET['raceId'] ?? '';
+
+// 2) Convert $type to enum
+$imageType = ImageType::tryFrom($type);
+if (!$imageType) {
+    echo json_encode(['error' => 'Invalid type']);
+    exit;
+}
+
+// 3) Fetch the Race from DB
+$raceRepository = $entityManager->getRepository(Race::class);
+/** @var Race|null $race */
+$race = $raceRepository->find($raceId);
+
+if (!$race) {
+    echo json_encode(['error' => 'Invalid Race ID']);
+    exit;
+}
+
+if ($imageType === ImageType::PORTRAIT) {
+  echo json_encode(['nextNumber' => $race->getPortraitNextNumber()]);
+} else {
+  echo json_encode(['nextNumber' => $race->getAvatarNextNumber()]);
+}
