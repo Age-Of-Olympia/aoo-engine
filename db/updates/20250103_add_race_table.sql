bcaad0c25197436d302afc33f169a7ebff69e845
Thomas Saquet <thomas@solution-libre.fr>
2025-01-05 00:11:59 +0000

    added race table

---
+++ db/updates/20250103_add_race_table.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS `races`;
+CREATE TABLE races (
+    id SERIAL PRIMARY KEY,
+    code VARCHAR(50) NOT NULL UNIQUE,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    playable BOOLEAN,
+    hidden BOOLEAN,
+    portraitNextNumber INT,
+    avatarNextNumber INT
+
+);
