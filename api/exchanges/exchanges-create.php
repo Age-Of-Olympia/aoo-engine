df94ddb79b45fe6f4de8fd71bdd06b8283e07738
Firefly74940 <guiot.sidney@gmail.com>
2025-01-09 23:58:14 +0000

    Api create exchange + fix front api base for edition

--- api/exchanges/exchanges-create.php
+++ api/exchanges/exchanges-create.php
@@ -1,30 +1,40 @@
 <?php
+require_once($_SERVER['DOCUMENT_ROOT'].'/config.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    
-    $objects = $_POST['objects'] ?? [];
-    $recipient = Player::get_player_by_name($_POST['recipient']);
 
-    $exchange = new Exchange();
-    $exchange->db->start_transaction('create_exchange');
-    try {
-      $exchange->create($player->id,$recipient->id);
+  if (!isset($_GET['targetId'])) {
 
-      foreach ($objects as $object) {
-          $decodedObject = json_decode($object, true);
-          $item = new Item($decodedObject['id']);
-          $count = abs($decodedObject['n']);
-          if(!$item->add_item($player, -$count, true))
-          {
-            throw new Exception('Erreur lors de l\'ajout de l\'objet à l\'échange');
-          }
-          $exchange->add_item_to_exchange($item->id, $count,$player->id);
-      }
-    } catch (Throwable $th) {
-      $exchange->db->rollback_transaction('create_exchange');
-      ExitError('Erreur lors de la création de l\'échange');
-    }
-    $exchange->db->commit_transaction('create_exchange');
-    ExitSuccess($exchange->db->get_last_id("items_exchanges"));
+    exit('error no merchant');
   }
-  ExitError('Invalid request');
+
+  $target = new Player($_GET['targetId']);
+  if (!$target->have_option('isMerchant')) {
+    exit('error not merchant');
+  }
+
+  $objects = $_POST['objects'] ?? [];
+  $recipient = Player::get_player_by_name($_POST['recipient']);
+
+  $exchange = new Exchange();
+  $exchange->db->start_transaction('create_exchange');
+  try {
+    $exchange->create($player->id, $recipient->id);
+
+    foreach ($objects as $object) {
+      $decodedObject = json_decode($object, true);
+      $item = new Item($decodedObject['id']);
+      $count = abs($decodedObject['n']);
+      if (!$item->add_item($player, -$count, true)) {
+        throw new Exception('Erreur lors de l\'ajout de l\'objet à l\'échange');
+      }
+      $exchange->add_item_to_exchange($item->id, $count, $player->id);
+    }
+  } catch (Throwable $th) {
+    $exchange->db->rollback_transaction('create_exchange');
+    ExitError('Erreur lors de la création de l\'échange');
+  }
+  $exchange->db->commit_transaction('create_exchange');
+  ExitSuccess($exchange->db->get_last_id("items_exchanges"));
+}
+ExitError('Invalid request');
