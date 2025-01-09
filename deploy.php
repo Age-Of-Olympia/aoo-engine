6393da7c960b8e21f93a9b099b77dd66c6f36aae
Thomas Saquet <thomas@solution-libre.fr>
2025-01-09 00:32:02 +0000

    correction chemin passphrase et comparaison de chaines

--- deploy.php
+++ deploy.php
@@ -11,9 +11,9 @@ include ('checks/admin-check.php');
 
 if (isset($_GET["type"]) && isset($_GET["passphrase"])) {
     echo "Deploying ".$_GET["type"];
-    $passPhrase = file_get_contents('~/etc/passphrase');
+    $passPhrase = file_get_contents('/home/'.get_current_user().'/etc/passphrase');
 
-    if ($passPhrase != "" && $passPhrase == $_GET["passphrase"]) {
+    if ($passPhrase != "" && strcmp($passPhrase, $_GET["passphrase"])) {
         $output=shell_exec("./scripts/deploy_".$_GET["type"].".sh 2>&1 | tee -a /tmp/deploy_".$_GET["type"].".log");
         echo "<br />".$output;
         echo "<br />Done.";
