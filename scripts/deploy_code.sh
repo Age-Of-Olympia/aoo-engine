d6ef7d8eaadd9bda87d61519c9d5591c868c1a3e
Thomas Saquet <thomas@solution-libre.fr>
2025-01-28 23:09:25 +0000

    ajout du repertoire checks dans les deploiements

--- scripts/deploy_code.sh
+++ scripts/deploy_code.sh
@@ -2,4 +2,4 @@ echo "$(date)<br>"
 cd ~/aoo-engine
 git pull \
 && git log --oneline -1 \
-&& cp -ra ~/aoo-engine/{*.php,admin,api,config,classes,css,js,scripts,src} ~/public_html/
+&& cp -ra ~/aoo-engine/{*.php,admin,api,checks,config,classes,css,js,scripts,src} ~/public_html/
