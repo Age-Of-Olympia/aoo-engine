f71434b2c2cc9021782589581b6a5dccd419bb3d
oubould <oubould@protonmail.com>
2025-02-20 13:56:51 +0100

    Retrait de l'ancienne db update

--- db/updates/20250122.add_coords_weather.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE `coords` ADD COLUMN `mask` varchar(35) DEFAULT NULL; 
-
-ALTER TABLE `coords` ADD COLUMN `scrollingMask` float(11); 
-
-ALTER TABLE `coords` ADD COLUMN `verticalScrolling` int(11); 
