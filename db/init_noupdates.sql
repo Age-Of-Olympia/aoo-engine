f65f45a29a31f042a7bacef2e3b531866aa88da4
Glenefal <glenefal@gmail.com>
2025-02-27 16:22:21 +0100

    feat(pji): maj du message de xp death distribution si pj inactif

--- db/init_noupdates.sql
+++ db/init_noupdates.sql
@@ -4348,7 +4348,8 @@ CREATE TABLE `players_kills` (
   `xp` int(11) NOT NULL DEFAULT 0,
   `assist` int(11) NOT NULL DEFAULT 0,
   `time` int(11) NOT NULL DEFAULT 0,
-  `plan` varchar(255) NOT NULL DEFAULT ''
+  `plan` varchar(255) NOT NULL DEFAULT '',
+  `is_inactive` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
