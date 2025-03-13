535b568dc4c037bbd2fd2d5a7f4a9bdfce44349e
Thomas Saquet <tsaquet@gmail.com>
2025-03-13 19:00:04 +0000

    tri correct des tags pour la release

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Récupérer le dernier tag et le tag précédent
-LAST_TAG=$(git tag --sort=-committerdate | tail -1)
-PREV_TAG=$(git tag --sort=-committerdate | tail -2 | head -1)
+LAST_TAG=$(git tag --sort=-committerdate | sort -V | tail -1)
+PREV_TAG=$(git tag --sort=-committerdate | sort -V | tail -2 | head -1)
 
 # Générer les release notes
 RELEASE_NOTES=$(git log $PREV_TAG...$LAST_TAG --pretty=format:"- %s")
