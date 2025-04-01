42a18ec046957ccbc42d0775795220d77eaa9747
Thomas Saquet <thomas@solution-libre.fr>
2025-04-01 20:35:31 +0000

    ajout d'une verification pour eviter de verifier les murs au CC

--- action.php
+++ action.php
@@ -8,29 +8,21 @@ use App\View\ActionResultsView;
 
 ob_start();
 
-
 /*
  * ACTION CHECK
  */
 
-
 // action
 if(!isset($_POST['action'])){
-
     exit('error action');
 }
 
 $actionJson = json()->decode('actions', $_POST['action']);
 
-
 // player
 $player = new Player($_SESSION['playerId']);
-
-
 $player->get_data();
-
 $player->get_caracs();
-
 
 // target
 if(!isset($_POST['targetId'])){
@@ -39,46 +31,37 @@ if(!isset($_POST['targetId'])){
 }
 
 $target = new Player($_POST['targetId']);
-
 $target->get_data();
-
 $target->get_caracs();
-
 
 // store target health
 $targetPvBefore = $target->getRemaining('pv');
 
 // healing a full life target
 if(!empty($actionJson->playerHeal)){
-
-
     if($targetPvBefore == $target->caracs->pv){
-
         exit('Ce personnage n\'a pas besoin de soins.');
     }
 }
 
 // player : ignore equipement
 if(!empty($actionJson->playerIgnore)){
-
-
     include('scripts/actions/playerIgnore.php');
 }
 
 // target : ignore equipement
 if(!empty($actionJson->targetIgnore)){
-
-
     include('scripts/actions/targetIgnore.php');
 }
 
 
 // distance
 $distance = View::get_distance($player->getCoords(), $target->getCoords());
-View::get_walls_between($player->coords, $target->coords);
+if ($distance > 0) {
+    View::get_walls_between($player->coords, $target->coords);
+}
 
 include('scripts/actions/check_max_spells.php');
-
 
 /*
  * action details
@@ -183,13 +166,11 @@ if($targetPvBefore != $targetPvAfter){
     $(document).ready(function(){
         var height = <?php echo $height ?>;
         if(height >= 225){
-
             $('.card-portrait').addClass('dead');
             $('#red-filter').hide();
         } else {
             $('#red-filter').css({'height':height +'px'});
         }
-
         $('body').append('<div class="clicked-cases-reseter" data-coords="<?php echo $target->coords->x .','. $target->coords->y ?>"></div>');
     });
     </script>
