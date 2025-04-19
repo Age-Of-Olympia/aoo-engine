555a70f2523d97def38f6e5bb2cbb909779c2ff9
tsaquet <thomas@solution-libre.fr>
2025-04-19 10:29:34 +0000

    une action peut provoquer la chute d'une arme avec un pourcentage de reussite

---
+++ src/Action/OutcomeInstruction/DropWeaponOutcomeInstruction.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Action\OutcomeInstruction;
+
+use App\Entity\OutcomeInstruction;
+use Doctrine\ORM\Mapping as ORM;
+use Item;
+use Player;
+use Str;
+
+#[ORM\Entity]
+class DropWeaponOutcomeInstruction extends OutcomeInstruction
+{
+    public function execute(Player $actor, Player $target): OutcomeResult {
+        $outcomeSuccessMessages = array();
+        $outcomeFailureMessages = array();
+        $params =$this->getParameters();
+        $result = false;
+        $targetLocation = $params["targetLocation"] ?? "";
+        $dropChance = $params["dropChance"] ?? 10;
+
+        $item = $target->emplacements->{$targetLocation};
+        
+        if(rand(1,100) <= $dropChance){
+            $target->drop($item, 1);
+            $resultText = "L'arme de votre adversaire tombe au sol.";
+            $outcomeSuccessMessages[sizeof($outcomeSuccessMessages)] = $resultText;
+            $result = true;
+        } else {
+            $resultText = "Votre adversaire était plus vigilent que prévu, son arme reste entre ses mains !";
+            $outcomeFailureMessages[sizeof($outcomeFailureMessages)] = $resultText;
+        }
+        
+        return new OutcomeResult($result, $outcomeSuccessMessages, $outcomeFailureMessages);
+    }
+
+}
