708ede4378a561d2782f629f56b09c0208d7e9dc
Firefly74940 <guiot.sidney@gmail.com>
2025-05-14 16:46:31 +0000

    fix __FILE__ / LINE With Backtrack feature :https://www.php.net/debug_backtrace

--- src/Service/AuditService.php
+++ src/Service/AuditService.php
@@ -20,7 +20,15 @@ class AuditService
     public function addAuditLog(?string $details): int
     {
         $audit = new Audit();
-        $audit->setAction(__FILE__.":".__LINE__);
+        $bt = debug_backtrace(2,1);
+        if(isset($bt[0]) && isset($bt[0]['file']) && isset($bt[0]['line'])){
+            $bt[0]['file'] = str_replace($_SERVER['DOCUMENT_ROOT'], '', $bt[0]['file']);
+            $audit->setAction($bt[0]['file'].":".$bt[0]['line']);
+        }
+        else{
+            $audit->setAction("Unknown");
+        }
+        
         $audit->setTimestamp(new \DateTime());
         if(isset($_SESSION['playerId'])){
           $audit->setUserId($_SESSION['playerId']);
