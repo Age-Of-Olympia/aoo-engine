e9b29f4bcc3aac840ff9bdfcab882c1c2e18dcb8
Thomas Saquet <thomas@solution-libre.fr>
2025-05-25 18:08:17 +0000

    fix: suppression de l'option pager de git pour la release s'il y a beaucoup de commits

--- scripts/generate-release-notes.sh
+++ scripts/generate-release-notes.sh
@@ -5,7 +5,7 @@ LAST_TAG=$(git tag --sort=-committerdate | sort -V | tail -1)
 PREV_TAG=$(git tag --sort=-committerdate | sort -V | tail -2 | head -1)
 
 # Générer les release notes
-RELEASE_NOTES=$(git log $PREV_TAG...$LAST_TAG --pretty=format:"- %s")
+RELEASE_NOTES=$(git --no-pager log $PREV_TAG...$LAST_TAG --pretty=format:"- %s")
 
 # Afficher les release notes
 echo "Release Notes for $LAST_TAG:"
