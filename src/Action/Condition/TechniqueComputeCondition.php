5a2c513fb066231c0173d59188c28cddf40bf444
Thomas Saquet <thomas@solution-libre.fr>
2025-05-25 14:31:55 +0000

    fix: mauvaise valeur de l'affichage du seuil pour qu'un sort passe

--- src/Action/Condition/TechniqueComputeCondition.php
+++ src/Action/Condition/TechniqueComputeCondition.php
@@ -12,13 +12,13 @@ class TechniqueComputeCondition extends ComputeCondition
     }
 
     protected function getDistanceTreshold() : int {
-        return floor(($this->distance) * 2.5);
+        return (4 * ($this->distance - 1));
     }
 
     protected function checkDistanceCondition(int $actorTotal): bool {
         $checkAboveDistance = true;
         if($this->distance > 1){
-            $distanceTreshold = 4 * ($this->distance - 1);
+            $distanceTreshold = $this->getDistanceTreshold();
             $checkAboveDistance = $actorTotal >= $distanceTreshold;
         }
         return $checkAboveDistance;
