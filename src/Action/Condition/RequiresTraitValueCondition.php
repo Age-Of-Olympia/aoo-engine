4d0943a134a00e3ad2c61af49f11ae0ef107ff1c
Firefly74940 <guiot.sidney@gmail.com>
2025-05-27 12:05:46 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Thomas Saquet <tsaquet@gmail.com>

--- src/Action/Condition/RequiresTraitValueCondition.php
+++ src/Action/Condition/RequiresTraitValueCondition.php
@@ -26,7 +26,7 @@ class RequiresTraitValueCondition extends BaseCondition
             }
             if ($key == "fatigue") {
                 if ($value == "fatigue_or_effects" && !$actor->data->fatigue && !$actor->have_effects_to_purge()) {
-                    array_push($details, "Vous n'êtes pas fatigué et n'avez aucun effet à purger!");
+                    array_push($details, "Vous n'êtes pas fatigué et n'avez aucun effet à purger !");
                     $costIsAffordable = false;
                     continue;
                 }
