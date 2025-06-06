39b4864c2dd1bd5887018a167ee6eba56451a30e
ovsmar <ovsep.simonian@gmail.com>
2025-06-06 20:52:54 +0200

    update port for e2e tests

--- selenium_tests/config/webdriverConfig.js
+++ selenium_tests/config/webdriverConfig.js
@@ -2,7 +2,7 @@ import { Builder, By, until } from 'selenium-webdriver';
 import chrome from 'selenium-webdriver/chrome.js';
 
 const CONFIG = {
-    baseUrl: process.env.CI ? 'http://selenium:4444/wd/hub' : 'http://localhost:80/',
+    baseUrl: process.env.CI ? 'http://selenium:4444/wd/hub' : 'http://localhost:9000/',
     timeouts: {
         implicit: 5000,
         explicit: 10000
