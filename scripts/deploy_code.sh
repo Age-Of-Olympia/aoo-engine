22968cb7f023c2aad4af9a0e698b067575ee8fe3
Firefly74940 <guiot.sidney@gmail.com>
2025-06-11 21:41:57 +0000

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Thomas Saquet <tsaquet@gmail.com>

--- scripts/deploy_code.sh
+++ scripts/deploy_code.sh
@@ -7,4 +7,4 @@ export HOME="/home/$(whoami)" \
 && cp -ra ~/aoo-engine/{composer.json,composer.lock} ~/public_html/
 && ~/bin/composer install \
 && ~/bin/composer dump-autoload -o \
-&& cp -ra ~/aoo-engine/{*.html,*.php,admin,api,checks,config,Classes,css,js,scripts,src,composer.json,composer.lock} ~/public_html/
+&& cp -ra ~/aoo-engine/{*.html,*.php,admin,api,checks,config,Classes,css,js,scripts,src} ~/public_html/
