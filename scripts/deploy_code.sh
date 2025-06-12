10d38bf313a70268cdd306bea7c02ad89b26d112
Firefly74940 <guiot.sidney@gmail.com>
2025-06-12 11:10:27 +0000

    Edit deploy_code.sh

--- scripts/deploy_code.sh
+++ scripts/deploy_code.sh
@@ -4,7 +4,7 @@ cd ~/aoo-engine \
 && git log --oneline -1
 export HOME="/home/$(whoami)" \
 && cd ~/public_html/ \
-&& cp -ra ~/aoo-engine/{composer.json,composer.lock} ~/public_html/
+&& cp -rav ~/aoo-engine/{composer.json,composer.lock} ~/public_html/
 && ~/bin/composer install \
 && ~/bin/composer dump-autoload -o \
 && cp -ra ~/aoo-engine/{*.html,*.php,admin,api,checks,config,Classes,css,js,scripts,src} ~/public_html/
