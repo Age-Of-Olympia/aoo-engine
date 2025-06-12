9e37ac2d36530439218756ffa4b0f3dbf86ccdc1
Firefly74940 <guiot.sidney@gmail.com>
2025-06-12 11:50:09 +0000

    fix line continuation

--- scripts/deploy_code.sh
+++ scripts/deploy_code.sh
@@ -4,10 +4,10 @@ cd ~/aoo-engine \
 && git log --oneline -1
 export HOME="/home/$(whoami)" \
 && cd ~/public_html/ \
-&& echo -e "copie des fichier composer :\n "
-&& cp -rav ~/aoo-engine/{composer.json,composer.lock} ~/public_html/
-&& echo -e "composer install + generation autoload :\n "
+&& echo -e "copie des fichier composer :\n " \
+&& cp -rav ~/aoo-engine/{composer.json,composer.lock} ~/public_html/ \
+&& echo -e "composer install + generation autoload :\n " \
 && ~/bin/composer install \
 && ~/bin/composer dump-autoload -o \
-&& echo -e "copie du reste des fichiers :\n "
+&& echo -e "copie du reste des fichiers :\n " \
 && cp -ra ~/aoo-engine/{*.html,*.php,admin,api,checks,config,Classes,css,js,scripts,src} ~/public_html/
