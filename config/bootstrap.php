9f1b0fd36eb50c1872dfa06f640a222e6f7181ab
Firefly74940 <guiot.sidney@gmail.com>
2025-06-18 15:28:02 +0000

    cleanup boostrat & orm config setup + update db constant exemple

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -8,12 +8,6 @@ use Doctrine\ORM\ORMSetup;
 require_once dirname(__FILE__)."/../vendor/autoload.php";
 require_once (__DIR__."/db_constants.php");
 
-// Create a simple "default" Doctrine ORM configuration for Attributes
-$orm_db_config = ORMSetup::createAttributeMetadataConfiguration(
-    paths: [__DIR__.'/../src/Entity'],
-    isDevMode: true,
-);
-$orm_db_config->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_FILE_NOT_EXISTS_OR_CHANGED);    
 
 //initialize the EntityManager allow shared connection for our transactions
 $em=EntityManagerFactory::getEntityManager();
