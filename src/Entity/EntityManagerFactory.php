eedf2528c206a8dce6e6c62f1baace6be8456819
Firefly74940 <guiot.sidney@gmail.com>
2025-06-18 15:28:02 +0000

    cleanup boostrat & orm config setup + update db constant exemple

--- src/Entity/EntityManagerFactory.php
+++ src/Entity/EntityManagerFactory.php
@@ -9,25 +9,38 @@ use Doctrine\ORM\ORMSetup;
 use Doctrine\DBAL\DriverManager;
 use Doctrine\ORM\Events;
 use Doctrine\ORM\Proxy\ProxyFactory;
+use Doctrine\ORM\Configuration;
 
 final class EntityManagerFactory
 {
     private static ?EntityManager $em = null;
-
+    private static ?Configuration $orm_db_config = null;
     public static function getEntityManager(): EntityManager
     {
         if (self::$em === null) {
-            $orm_db_config = ORMSetup::createAttributeMetadataConfiguration(
-                paths: [__DIR__],
-                isDevMode: false
-            );
-            $orm_db_config->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_FILE_NOT_EXISTS_OR_CHANGED);
-            $connection = DriverManager::getConnection(DB_CONSTANTS, $orm_db_config);
-            self::$em = new EntityManager($connection, $orm_db_config);
+            EntityManagerFactory::InitOrmConfig();
+            $connection = DriverManager::getConnection(DB_CONSTANTS, self::$orm_db_config);
+            self::$em = new EntityManager($connection, self::$orm_db_config);
         }
         $eventManager = self::$em->getEventManager();
         $eventManager->addEventListener(Events::loadClassMetadata, new ActionMetadataListener());
         $eventManager->addEventListener(Events::loadClassMetadata, new OutcomeInstructionMetadataListener());
         return self::$em;
+    }
+
+    public static function InitOrmConfig(): void
+    {
+        if (self::$orm_db_config !== null) {
+            return; // already initialized
+        }
+        $isDevMode = defined('DEV_MODE') && DEV_MODE;
+
+        self::$orm_db_config = ORMSetup::createAttributeMetadataConfiguration(
+                paths: [__DIR__],
+                isDevMode: $isDevMode
+            );
+        self::$orm_db_config->setAutoGenerateProxyClasses($isDevMode ? ProxyFactory::AUTOGENERATE_FILE_NOT_EXISTS_OR_CHANGED : ProxyFactory::AUTOGENERATE_NEVER);
+        self::$orm_db_config->setProxyDir(__DIR__ . '/Proxy');
+        self::$orm_db_config->setProxyNamespace('App\Proxy');
     }
 }
