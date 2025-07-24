0b9f2f747c20a1f1d2729cef15aa73e97d35b0ca
oubould <oubould@protonmail.com>
2025-07-24 02:03:27 +0200

    Petite modification du HealingOutcome pour pouvoir prendre des valeurs fixes et gérer le mana

--- src/Action/OutcomeInstruction/HealingOutcomeInstruction.php
+++ src/Action/OutcomeInstruction/HealingOutcomeInstruction.php
@@ -15,14 +15,29 @@ class HealingOutcomeInstruction extends OutcomeInstruction
         // e.g. { "actorHealingTrait": "agi" }, { "actorHealingTrait": "agi", "bonusHealingTrait" : "3" }
         $actorTraitHealing = $this->getParameters()['actorHealingTrait'] ?? 0;
         $bonusTraitHealing = $this->getParameters()['bonusHealingTrait'] ?? 0;
+        $actorTraitPMHealing = $this->getParameters()['actorPMHealingTrait'] ?? 0;
+        $bonusTraitPMHealing = $this->getParameters()['bonusPMHealingTrait'] ?? 0;
         $outcomeSuccessMessages = array();
         if(!empty($actorTraitHealing)){
-            $baseHeal = (is_numeric($actorTraitHealing)) ? $actorTraitHealing : $actor->caracs->{$actorTraitHealing};
-            $bonusHeal = (is_numeric($bonusTraitHealing)) ? $bonusTraitHealing : $actor->caracs->{$bonusTraitHealing};
+            $baseHeal = is_numeric($actorTraitHealing) ? $actorTraitHealing : $actor->caracs->{$actorTraitHealing};
+            $bonusHeal = ($bonusTraitHealing  == 0) ? $bonusTraitHealing : $actor->caracs->{$bonusTraitHealing};
             $healing = $baseHeal + $bonusHeal;
             $target->putBonus(array('pv'=>$healing));
             $outcomeSuccessMessages[0] = 'Vous soignez '. $healing .' points de vie à '. $target->data->name.'.';
-            $outcomeSuccessMessages[1] = CARACS[$actorTraitHealing] .' = '. $baseHeal;
+            $outcomeSuccessMessages[1] = is_numeric($actorTraitHealing) ? "Valeur fixe à " . $actorTraitHealing . '.' : CARACS[$actorTraitHealing] .' = '. $baseHeal;
+            if ($bonusHeal > 0) {
+                $outcomeSuccessMessages[1] .= ' + '. $bonusHeal;
+            }
+        
+        } 
+        
+        if(!empty($actorTraitPMHealing)){
+            $baseHeal = is_numeric($actorTraitPMHealing) ? $actorTraitPMHealing : $actor->caracs->{$actorTraitPMHealing};
+            $bonusHeal = ($bonusTraitPMHealing == 0) ? $bonusTraitPMHealing : $actor->caracs->{$bonusTraitPMHealing};
+            $healing = $baseHeal + $bonusHeal;
+            $target->putBonus(array('pm'=>$healing));
+            $outcomeSuccessMessages[0] = 'Vous rendez '. $healing .' points de mana à '. $target->data->name.'.';
+            $outcomeSuccessMessages[1] = is_numeric($actorTraitPMHealing) ? "Valeur fixe à " . $actorTraitPMHealing . '.' : CARACS[$actorTraitPMHealing] .' = '. $baseHeal;
             if ($bonusHeal > 0) {
                 $outcomeSuccessMessages[1] .= ' + '. $bonusHeal;
             }
