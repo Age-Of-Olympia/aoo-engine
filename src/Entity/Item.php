e665a1566e0b21d3c7a4ccf2d8022b64a74e956e
Firefly74940 <guiot.sidney@gmail.com>
2025-08-27 18:00:13 +0000

    passe item as an entity for better compat

--- src/Entity/Item.php
+++ src/Entity/Item.php
@@ -18,23 +18,24 @@ class Item
     #[ORM\Column(type: "string", length: 255)]
     private ?string $name = null;
 
-//    #[ORM\Column(type: "boolean")]
-//     private bool $private = false;
+   #[ORM\Column(type: "boolean")]
+    private bool $private = false;
 
-//     #[ORM\Column(type: "boolean")]
-//     private bool $enchanted = false;
+    #[ORM\Column(type: "boolean")]
+    private bool $enchanted = false;
 
-//     #[ORM\Column(type: "boolean")]
-//     private bool $vorpal = false;
+    #[ORM\Column(type: "boolean")]
+    private bool $vorpal = false;
 
-//     #[ORM\Column(type: "boolean")]
-//     private bool $cursed = false;
+    #[ORM\Column(type: "boolean")]
+    private bool $cursed = false;
 
-//     #[ORM\Column(type: "string", length: 255)]
-//     private string $element = '';
+    #[ORM\Column(type: "string", length: 255)]
+    private string $element = '';
 
-//     #[ORM\Column(type: "string", length: 255)]
-//     private ?string $spell= null;
+    #[ORM\Column(type: "string", length: 255)]
+    private ?string $spell= null;
+
     //getters and setters
     public function getId(): ?int
     {
@@ -51,6 +52,62 @@ class Item
     public function setName(string $name): void
     {
         $this->name = $name;
+    }
+
+    public function isPrivate(): bool
+    {
+        return $this->private;
+    }
+    public function setPrivate(bool $private): void
+    {
+        $this->private = $private;  
+    }
+
+    public function isEnchanted(): bool
+    {
+        return $this->enchanted;
+    }
+    public function setEnchanted(bool $enchanted): void
+    {
+        $this->enchanted = $enchanted;
+    }
+
+    public function isVorpal(): bool
+    {
+        return $this->vorpal;
+    }
+    public function setVorpal(bool $vorpal): void
+    {
+        $this->vorpal = $vorpal;
+    }
+
+    public function isCursed(): bool
+    {
+        return $this->cursed;
+    }
+    public function setCursed(bool $cursed): void
+    {
+        $this->cursed = $cursed;
+    }
+
+    public function getElement(): string
+    {
+        return $this->element;
+    }
+
+    public function setElement(string $element): void
+    {
+        $this->element = $element;
+    }
+
+    public function getSpell(): ?string
+    {
+        return $this->spell;
+    }
+
+    public function setSpell(?string $spell): void
+    {
+        $this->spell = $spell;
     }
 }
 
