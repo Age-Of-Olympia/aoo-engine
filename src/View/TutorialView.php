84c03b2286c492114aee66b278f89892cce0eb5a
tsaquet <thomas@solution-libre.fr>
2025-11-21 17:06:20 +0000

    fix(view): update tutorial and menu views
    - Improve tutorial view rendering
    - Update menu view for tutorial integration

--- src/View/TutorialView.php
+++ src/View/TutorialView.php
@@ -15,6 +15,10 @@ class TutorialView
 
         $player->getCoords();
 
+        // Safety check for null coords
+        $coordX = $player->coords->x ?? 0;
+        $coordY = $player->coords->y ?? 0;
+
         echo '
 <div id="tooltip"><div class="text">tooltip</div><div class="tooltip-next"><a href="#" class="next">[suite]</a></div></div>
 ';
@@ -22,15 +26,15 @@ class TutorialView
 ?>
         <script>
             window.playerId = <?php echo $_SESSION['playerId'] ?>;
-            window.dataCoords = "<?php echo $player->coords->x + 1 ?>,<?php echo $player->coords->y ?>";
+            window.dataCoords = "<?php echo $coordX + 1 ?>,<?php echo $coordY ?>";
         </script>
 
         <!-- Tutorial CSS -->
-        <link href="css/tutorial/tutorial.css?v=20251119a" rel="stylesheet">
+        <link href="css/tutorial/tutorial.css?v=20251121a" rel="stylesheet">
 
         <!-- Tutorial JavaScript - NEW MODULAR SYSTEM -->
         <script src="js/tutorial/TutorialPositionManager.js?v=20251118a"></script>
-        <script src="js/tutorial/TutorialUI.js?v=20251120a"></script>
+        <script src="js/tutorial/TutorialUI.js?v=20251121i"></script>
         <script src="js/tutorial/TutorialHighlighter.js?v=20251118a"></script>
         <script src="js/tutorial/TutorialTooltip.js?v=20251118a"></script>
         <script src="js/tutorial/TutorialInit.js?v=20251120a"></script>
