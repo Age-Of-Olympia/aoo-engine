3c04c05dc5afef0c6bcb9cd51896ad6b0d885f2a
tsaquet <thomas@solution-libre.fr>
2025-11-26 16:59:51 +0000

    chore(tutorial): update tooltip script version for cache busting
    Update TutorialTooltip.js version to 20251126h to ensure browsers
    load the latest version with positioning fixes.

--- src/View/TutorialView.php
+++ src/View/TutorialView.php
@@ -30,13 +30,13 @@ class TutorialView
         </script>
 
         <!-- Tutorial CSS -->
-        <link href="css/tutorial/tutorial.css?v=20251122s" rel="stylesheet">
+        <link href="css/tutorial/tutorial.css?v=20251125M" rel="stylesheet">
 
         <!-- Tutorial JavaScript - NEW MODULAR SYSTEM -->
         <script src="js/tutorial/TutorialPositionManager.js?v=20251118a"></script>
         <script src="js/tutorial/TutorialUI.js?v=20251122p"></script>
-        <script src="js/tutorial/TutorialHighlighter.js?v=20251122c"></script>
-        <script src="js/tutorial/TutorialTooltip.js?v=20251122s"></script>
+        <script src="js/tutorial/TutorialHighlighter.js?v=20251125e"></script>
+        <script src="js/tutorial/TutorialTooltip.js?v=20251126h"></script>
         <script src="js/tutorial/TutorialInit.js?v=20251122d"></script>
 <?php
     }
