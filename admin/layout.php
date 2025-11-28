092a90bd563c15f8c181e42c3b2d7927df6b4c67
tsaquet <thomas@solution-libre.fr>
2025-11-28 11:42:00 +0000

    chore(css): update cache-busting version to 20251128
    Update CSS version parameter across all layouts to force
    browser cache refresh for recent styling changes.

--- admin/layout.php
+++ admin/layout.php
@@ -36,7 +36,7 @@ function admin_layout($title, $content) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>$title - Admin of Olympia</title>
-    <link href="/css/main.min.css?v=20251020" rel="stylesheet">
+    <link href="/css/main.min.css?v=20251128" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
         /* Reset body constraints from main.min.css */
