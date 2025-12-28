cb8d6f870b9ae3a30379ee260f16fd247c4eefd1
tsaquet <thomas@solution-libre.fr>
2025-12-28 10:11:26 +0000

    chore(cache): bump tutorial JS version to 20251228
    Force browser cache refresh for tutorial JavaScript changes

--- Classes/Ui.php
+++ Classes/Ui.php
@@ -47,7 +47,7 @@ class Ui{
         }
 
         // Tutorial System (feature-flagged for specific players)
-        $tutorialVersion = '20251130';
+        $tutorialVersion = '20251228';
         echo '
                 <!-- Modal System -->
                 <link href="css/modal.css?v=20251112" rel="stylesheet">
