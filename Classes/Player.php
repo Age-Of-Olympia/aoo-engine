d9b0ae5f01d8f0e8f87deefdf82004135fc38b53
Firefly74940 <guiot.sidney@gmail.com>
2026-03-13 16:04:59 +0000

    Fix PI loss

--- Classes/Player.php
+++ Classes/Player.php
@@ -741,7 +741,12 @@ class Player implements ActorInterface {
 
         // Ajout d'un cap temporaire des PIs pour la fin de la saison 1
         $xpCap = SEASON_XP;
-        $pi = min(max(0,($xpCap - $this->data->xp)),$xp);
+        if($xp>0){
+            $pi = min(max(0,($xpCap - $this->data->xp)),$xp);
+        }
+        else{
+            $pi = max(min(0,$xpCap-$this->data->xp-$xp) ,$xp);
+        }
         $this->data->xp += $xp;
         $this->data->pi += $pi;
 
