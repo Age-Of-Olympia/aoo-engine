d3b7a3e5fae82e9101e214d4f5bc464057e7bf2e
tsaquet <thomas@solution-libre.fr>
2026-07-05 01:36:02 +0200

    refactor(account): extract AccountView from account.php
    Les options du profil deviennent App\View\AccountView (buildOptions
    partagé avec le handler POST de bascule, render pour l'affichage),
    appelées par account.php — comportement inchangé — et réutilisables
    en fragment par le panneau du HUD.

--- account.php
+++ account.php
@@ -1,6 +1,7 @@
 <?php
 use App\Factory\PlayerFactory;
 use App\Service\AccountDeletionService;
+use App\View\AccountView;
 use Classes\Ui;
 use Classes\Str;
 use Classes\File;
@@ -62,43 +63,7 @@ if(isset($_POST['changeName'])){
     exit();
 }
 
-// Build options array dynamically
-$options = array(
-    'changeMail'=>"Changer Mail<br /><sup>" .
-        (!empty($player->data->plain_mail) ? htmlspecialchars($player->data->plain_mail) : "") . "</sup>",
-    'changePortrait'=>"Changer de Portrait<br /><sup>Vous pouvez faire une demande de Portrait sur le <a href='https://age-of-olympia.net/forum.php?topic=1725177169' target='_blank'>forum</a></sup>",
-    'changeAvatar'=>"Changer d'Avatar<br /><sup>Vous pouvez faire une demande d'Avatar sur le <a href='https://age-of-olympia.net/forum.php?topic=1725177169' target='_blank'>forum</a></sup>",
-    'changeMdj'=>"",
-    'changeStory'=>"",
-    'raceHint'=>"Indice de Race<br /><sup>Affiche une bordure de couleur autour du personnage</sup>",
-    'raceHintMax'=>"Indice de Race maximale<br /><sup>Colore également l'arrière plan du personnage</sup>",
-    'showBlockedTiles'=>"Marquer les cases infranchissables<br /><sup>Affiche un ⛔ sur les cases (murs, joueurs, cases interdites) où vous ne pouvez pas vous déplacer</sup>",
-    // 'noPrompt'=>"Désactiver le système anti-misslick<br /><sup>Vous n'aurez plus d'alertes pour confirmer vos Actions</sup>",
-    'hideGrid'=>"Cacher le damier de la Vue<br /><sup>La grille ne s'affichera plus</sup>",
-    'noMask'=>"Désactiver les masques<br /><sup>Les effets de brumes et de pluie ne s'afficheront plus</sup>",
-    'showActionDetails'=>"Afficher les détails des Actions<br /><sup>Affiche les calculs et les jets</sup>",
-    'newHud'=>"Nouvelle interface (bêta)<br /><sup>Nouvelle disposition du jeu sur grand écran : barre de statut, rail de navigation, chat et évènements</sup>",
-    'noTrain'=>"Interdire les entraînements<br />",
-    'dlag'=>"DLA glissante<br /><sup>Décale l'heure du prochain tour</sup>",
-    'deleteAccount'=>"Demander la suppression du compte<br /><sup>Votre compte sera supprimé sous 7 jours</sup>",
-    'reloadView'=>"Rafraichir la Vue<br /><sup>Si cette dernière est buguée</sup>",
-    'incognitoMode'=>"Mode Incognito (PNJ)<br /><sup>Invisible sur la carte et dans les évènements</sup>",
-    'anonymeMode'=>"Mode Incognito/Anonyme (PNJ)<br /><sup>Non trouvable dans les recherches de destinataires</sup>",
-);
-
-// Conditionally add tutorial replay option
-// Show for players who have completed tutorial before (regardless of feature flag)
-$db = new Db();
-$sessionManager = new TutorialSessionManager($db);
-$hasCompletedTutorial = $sessionManager->hasCompletedBefore($player->id);
-
-// Always show replay option if player has completed tutorial
-// The feature flag will determine which tutorial system to use (new vs old)
-// NPCs (negative ids) never get the option — it's not meaningful for them
-// and the legacy fallback would otherwise activate via the !isEnabled branch.
-if ($player->id >= 0 && ($hasCompletedTutorial || !TutorialFeatureFlag::isEnabledForPlayer($player->id))) {
-    $options['showTuto'] = "Rejouer le tutoriel";
-}
+$options = AccountView::buildOptions($player);
 
 define('OPTIONS', $options);
 
@@ -156,162 +121,9 @@ if(!empty($_POST['option'])){
 }
 
 
-echo '<a href="index.php"><button><span class="ra ra-sideswipe"></span> Retour</button></a>';
-echo '<button data-change="name">Changer Nom</button>';
-echo '<a href="account.php?changePsw"><button>Changer Mot de Passe</button></a>';
-
-echo '
-<table border="1" align="center" class="marbre">';
-
-echo '<tr><th colspan="2" align="center">Options du Profil</th></tr>';
-
-
-$checked = array();
-
-
-foreach(OPTIONS as $k=>$e){
-
-
-    $checked[$k] = '';
-}
-
-foreach($player->get_options() as $e){
-
-
-    $checked[$e] = 'checked';
-}
-
-
-foreach(OPTIONS as $k=>$e){
-
-
-    if(($k =='incognitoMode' || $k =='anonymeMode' ) && $player->id>=0)
-    { //Option non disponible pour les PJ
-        continue;
-    }
-    else{
-        echo '<tr>';    
-    }
-    echo '
-    
-        <td>
-            ';
-
-            if($k == 'changeMdj'){
-
-                echo "Modifier son MDJ<br /><sup>";
-                echo explode("\n", $player->data->text)[0] .' [...]';
-                echo '</sup>';
-            }
-            elseif($k == 'changeStory'){
-
-                echo "Modifier son Histoire<br /><sup>";
-                echo explode("\n", $player->data->story)[0] .' [...]';
-                echo '</sup>';
-            }
-            elseif($k == 'manageUploads'){
-
-                echo $e .'<br />';
-
-                // drag and drop for upload img
-                $uploadedN = count(File::get_uploaded($player));
-                $uploadMax = File::get_uploaded_max($player);
-
-                echo '<sup>Vous avez uploadé '. $uploadedN .'/'. $uploadMax .' images</sup>';
-            }
-            else{
-
-                echo $e;
-            }
-
-            echo '
-        </td>
-        <td>';
-
-            if($k == 'changePortrait'){
-
-                echo '
-                <a href="account.php?portraits"><img src="'. $player->data->mini .'" /></a>
-                ';
-            }
-            elseif($k == 'changeAvatar'){
-
-                echo '
-                <a href="account.php?avatars"><img src="'. $player->data->avatar .'" width="50" /></a>
-                ';
-            }
-            elseif($k == 'changeMdj'){
-
-                echo '
-                <a href="account.php?mdj"><button>Changer</button></a>
-                ';
-            }
-            elseif($k == 'changeStory'){
-
-                echo '
-                <a href="account.php?story"><button>Changer</button></a>
-                ';
-            }
-            elseif($k == 'showTuto'){
-                // Feature flag determines which tutorial system to use
-                if (TutorialFeatureFlag::isEnabledForPlayer($player->id)) {
-                    // New tutorial system - use URL parameter to trigger replay
-                    echo '
-                    <a href="index.php?replay_tutorial=1"><button style="width: 100%;">Tutoriel</button></a>
-                    ';
-                } else {
-                    // Old tutorial system
-                    echo '
-                    <a href="index.php?tutorial"><button style="width: 100%;">Tutoriel</button></a>
-                    ';
-                }
-            }
-            elseif($k == 'changeMail'){
-                // Disable email change for PNJs
-                if($player->id > 0) {
-                    echo '<a href="account.php?changeMail"><button style="width: 100%;">Changer</button></a>';
-                } else {
-                    echo '<button style="width: 100%; opacity: 0.5; cursor: not-allowed;" disabled>PNJ - Non disponible</button>';
-                }
-            }
-            else{
-
-                echo '
-                <input type="checkbox" class="option" data-option="'. $k .'" '. $checked[$k] .' />
-                ';
-            }
-
-            echo '
-        </td>
-    </tr>
-    ';
-
-}
-
-
-
-if($player->have_option('isAdmin')){
-
-
-    echo '
-    <tr>
-        <td>Ouvrir la console (admin)</td>
-        <td><input type="button" OnClick="create_console(); document.getElementById(\'input-line\').focus()" value="Ouvrir" style="width: 100%;" /></td>
-    </tr>
-    ';
-}
-
-
-echo '
-</table>
-';
+AccountView::render($player, $options);
 
 ?>
-<script>
-    window.alreadyChanged = <?php if($player->have_option('alreadyChanged')) echo 1; else echo 0; ?>;
-    window.oldName = "<?php echo $player->data->name ?>";
-</script>
-<script src="js/account.js"></script>
 
 
 <?php
